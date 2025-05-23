430b939b26250caf7d6a33e98e5b16b5d01ee147
1F10230110 <s1f102301101@iniad.org>
2025-05-24 03:02:18 +0900

    topページにアニメーションを追加

--- timetable/static/timetable/js/index.js
+++ timetable/static/timetable/js/index.js
@@ -1,3 +1,4 @@
+// 文字表示
 document.addEventListener('DOMContentLoaded', () => {
   const text = document.querySelector('.text');
   const link = document.querySelector('.link');
@@ -13,3 +14,30 @@ document.addEventListener('DOMContentLoaded', () => {
     link.classList.add('is-shown');
   }, 1000); 
 });
+
+// バブル
+function getRandomColor() {
+      const colors = [
+        'rgba(250, 174, 255, 0.4)',
+        'rgba(83, 226, 255, 0.42)', 
+        'rgba(133, 135, 255, 0.3)',
+      ];
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
+
+    for (let i = 0; i < 15; i++) {
+      const bubble = document.createElement('div');
+      bubble.className = 'bubble';
+
+      const size = Math.random() * 250 + 150;
+      bubble.style.width = `${size}px`;
+      bubble.style.height = `${size}px`;
+
+      bubble.style.left = `${Math.random() * 100}vw`;
+      bubble.style.backgroundColor = getRandomColor();
+
+      bubble.style.animationDelay = `${Math.random() * 10}s`;
+      bubble.style.animationDuration = `${8 + Math.random() * 5}s`;
+
+      document.body.appendChild(bubble);
+    }
